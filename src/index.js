fb3e1c45a267ca2f1ffc180a73c72c9a9e1713e4
Cristina Verdi <Cristina @code-sherpas>
2022-07-01 16:40:05 +0200

    create product

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@ const app = require('./app');
 const port = '8888';
 
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}...`);
-});
+ 
+}); 
+
+// declarar una clase
+class Product {
+  constructor() {}
+}
+
+// instancio una clas
+Product(sellIn)
+
+
+function increaseQuality(number) {} // declaro una función
+
+increaseQuality(3) // invoco, llamo, uso, ejecuto una función
