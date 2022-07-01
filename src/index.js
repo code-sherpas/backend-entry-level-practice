69cb8296dc7ea8df48c7dcbb9948f25a2b31700a
David Gracia <david.gracia@code-sherpas.rocks>
2022-07-01 17:18:14 +0200

    descrease sell in

--- src/index.js
+++ src/index.js
@@ -8,13 +8,30 @@ app.listen(port, () => {
 
 // declarar una clase
 class Product {
-  constructor() {}
+  constructor(sellIn, quality) {
+    this.sellIn = sellIn;
+    this.quality = quality;
+  }
+
+  decreaseSellIn(valor) {
+    this.sellIn = this.sellIn - valor
+  }
 }
 
 // instancio una clas
-Product(sellIn)
+let product = new Product(5,10)
 
+console.log("Before decreasing")
+console.log(product.sellIn)
+console.log(product.quality)
 
-function increaseQuality(number) {} // declaro una función
+product.decreaseSellIn(1)
+
+console.log("After decreasing")
+console.log(product.sellIn)
+console.log(product.quality)
+
+/*function increaseQuality(number) {} // declaro una función
 
 increaseQuality(3) // invoco, llamo, uso, ejecuto una función
+*/
