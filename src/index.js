834850458de7297d2074d8139cc41ba9523142f0
David Gracia Celemendi <david.gracia.celemendi@gmail.com>
2022-07-11 14:04:55 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,33 +5,3 @@ const port = '8888';
 app.listen(port, () => {
  
 }); 
-
-// declarar una clase
-class Product {
-  constructor(sellIn, quality) {
-    this.sellIn = sellIn;
-    this.quality = quality;
-  }
-
-  decreaseSellIn(valor) {
-    this.sellIn = this.sellIn - valor
-  }
-}
-
-// instancio una clas
-let product = new Product(5,10)
-
-console.log("Before decreasing")
-console.log(product.sellIn)
-console.log(product.quality)
-
-product.decreaseSellIn(1)
-
-console.log("After decreasing")
-console.log(product.sellIn)
-console.log(product.quality)
-
-/*function increaseQuality(number) {} // declaro una función
-
-increaseQuality(3) // invoco, llamo, uso, ejecuto una función
-*/
